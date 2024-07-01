5bca6673443133b8e857905418474113a2419770
jig <jig@saw.com>
2024-07-01 08:40:38 -0300

    gerenciamento_biblioteca finished

--- 2_bim/gerenciamento_biblioteca.py
+++ 2_bim/gerenciamento_biblioteca.py
@@ -1,32 +1,46 @@
 class Livro:
     def __init__(self,titulo,autor,ano):
-        self.titulo = titulo
-        self.autor = autor
-        self.ano = ano
+        self._titulo = titulo
+        self._autor = autor
+        self._ano = ano
+        self.livros = []
 
-        @property
-        def titulo(self):
-            return self.titulo
-        
-        @property
-        def autor(self):
-            return self.autor
-        
-        @property
-        def ano(self):
-            return self.ano
-        
-        @property
-        def livros(self):
-            return self.titulo
-            return self.autor
-            return self.ano
-        
-        def listar_livros(self):
-            for livro in self.livros:
-                print(f"{livro.titulo}, {livro.autor}, {livro.ano}")
-                
-        
-        def adicionar_livro(self, livro):
-            self.livros.append(livro)
+        # GETTERS
 
+    @property
+    def titulo(self):
+        return self._titulo
+        
+    @property
+    def autor(self):
+        return self._autor
+        
+    @property
+    def ano(self):
+        return self._ano
+        
+        # SETTERS
+
+    @titulo.setter
+    def titulo(self, titulo):
+        self._titulo = titulo
+
+    @autor.setter
+    def autor(self, autor):
+        self._autor = autor
+
+    @ano.setter
+    def ano(self,ano):
+        self._ano = ano
+
+    def adicionar_livro(self, livro):
+        self.livros.append(livro)   
+
+    def listar_livro(self):
+        for livro in self.livros: 
+            print(f"{livro['titulo']}, {livro['autor']}, {livro['ano']}")        
+
+bkl = Livro("Titulo", "Autor", 2023)
+bkl.adicionar_livro({"titulo" : "1984", "autor": "George Orwell", "ano": 1949})
+bkl.adicionar_livro({"titulo" : "I, Robot", "autor": "Isaac Asimov", "ano": 1950})
+bkl.listar_livro()
---
+++ 2_bim/testing_2.py
@@ -0,0 +1,12 @@
+books_dict = {
+    "title": "1984",
+    "title": "I, Robot",
+    "author": "George Orwell",
+    "author": "Isaac Asimov",
+    "year": "1918",
+    "year": "2005"
+
+}
+
+for x in books_dict:
+    print(books_dict)
